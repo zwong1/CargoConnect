fcbc806be28f756b0aba3205139c0fde87dd8fd3
Robotics <RoscoeRobotics@gmail.com>
2021-10-25 11:05:22 -0500

    Saturday Work

--- LineFollowingFunctions.C
+++ LineFollowingFunctions.C
@@ -1,5 +1,5 @@
 //----------------------------------------------------------------------------------------------------
-// Name: 
+// Name:
 //----------------------------------------------------------------------------------------------------
 // Description:
 //
@@ -68,6 +68,127 @@ void lineFollowForDistance(float rotations, float speed, int colorSensorToUse, i
 
 		setMotorSpeed(leftDrive, speedLeft);
 		setMotorSpeed(rightDrive, speedRight);
+
+	}
+
+
+	turnOffDriveMotors();
+
+	setBrakeMode(brakeMode);
+
+
+}
+
+
+
+
+
+//----------------------------------------------------------------------------------------------------
+// Name:
+//----------------------------------------------------------------------------------------------------
+// Description:
+//
+//----------------------------------------------------------------------------------------------------
+//Inputs:
+//
+//----------------------------------------------------------------------------------------------------
+void lineFollowUntilLine(float speed, int colorSensorToUse, int edgeToUse, bool brakeMode, int colorToLookFor)
+
+{
+
+	// Dimensioning variables
+	float error;
+	float correctionFactor;
+	float speedLeft;
+	float speedRight;
+	float gain;
+	float colorSensorReading;
+	float midPoint;
+	float lightLevelToLookFor;
+	float colorSensorDetecting;
+
+	midPoint = (reflectedLightIntensityOnBlack + reflectedLightIntensityOnWhite)/2;
+	gain = .01 * speed;
+
+
+
+
+	if (colorToLookFor == 1)     // Look for white
+  {
+    lightLevelToLookFor = reflectedLightIntensityOnWhite - 10;
+  }
+  else
+  {
+    lightLevelToLookFor = reflectedLightIntensityOnBlack + 10;
+  }
+
+
+
+
+
+	if (colorSensorToUse == 1)				// Use the left color sensor
+		{
+			colorSensorDetecting = getColorReflected(rightColor);
+		}
+		else														// Use the right color sensor
+		{
+			colorSensorDetecting = getColorReflected(leftColor);
+		}
+
+
+
+
+
+	// move forward until the encoder value is greater then the degrees to move
+	while (((colorToLookFor == 1) && ( colorSensorDetecting < lightLevelToLookFor)) + ((colorToLookFor != 1) && ( colorSensorDetecting > lightLevelToLookFor)))
+	{
+
+
+
+		if (colorSensorToUse == 1)				// Use the left color sensor
+		{
+			colorSensorReading = getColorReflected(leftColor);
+		}
+		else														// Use the right color sensor
+		{
+			colorSensorReading = getColorReflected(rightColor);
+		}
+
+
+		// Setpoint that we want to go in - actual current reading
+		error = midPoint - colorSensorReading;
+
+		// Compute the correction factor
+		correctionFactor = error * gain;
+
+		// detecting what edge you are on
+		if (edgeToUse == 1)		// 1 = left edge
+		{
+			speedLeft = speed - correctionFactor;
+			speedRight = speed + correctionFactor;
+		}
+		else
+		{
+			speedLeft = speed + correctionFactor;
+			speedRight = speed - correctionFactor;
+
+		}
+
+		setMotorSpeed(leftDrive, speedLeft);
+		setMotorSpeed(rightDrive, speedRight);
+
+
+		if (colorSensorToUse == 1)				// Use the left color sensor
+		{
+			colorSensorDetecting = getColorReflected(rightColor);
+		}
+		else														// Use the right color sensor
+		{
+			colorSensorDetecting = getColorReflected(leftColor);
+		}
+
+
+
 
 	}
 
